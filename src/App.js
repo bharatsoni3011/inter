cd1e877dea186c8171f99c5c6cb73c831f0de793
Bharat Soni <bharat@pop-os.localdomain>
2022-04-24 18:07:31 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+// import './App.css';
+// import React from 'react';
+import ReactECharts from 'echarts-for-react';
+const r=(a,b)=>{
+  return Math.floor(Math.random() * (b - a + 1) ) + a;
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const options = {
+    // grid: { top: '80em', right: '100em', bottom: '80em', left: '100em' },
+    xAxis: [{
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    }],
+    yAxis: [{
+      name:'Precipitation ',
+      type: 'value',
+      axisLabel: {
+        formatter: `{value} ml`
+      }
+    },{
+      name:'Temperature',
+      type:'value',
+      axisLabel: {
+        formatter: '{value} °C'
+      }
+    }],
+    legend: {
+      data : [ 'Evaporation','Precipitation','Temperature'] 
+    },
+    series: [
+      {
+        name:'Evaporation',
+        data: Array.from({length:7},()=>r(30,250)),
+        type: 'bar',
+      },
+      {
+        name:'Precipitation',
+        data: Array.from({length:7},()=>r(30,250)),
+        type: 'bar',
+      },
+      {
+        yAxisIndex:1,
+        name:'Temperature',
+        data: Array.from({length:8},()=>r(10,25)),
+        type: 'line',
+        
+        // smooth: true,
+      },
+    ],
+    
+  };
+
+  return <ReactECharts option={options} />;
 }
 
 export default App;
